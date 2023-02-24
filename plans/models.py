5099e8ed9053d9855e8bd8d0ba9be9fce9bdb691
Welley <monteirowelley7@gmail.com>
2023-02-23 21:55:29 -0300

    Update Adventure Models

--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Plans(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=300)
+    type_plan = models.CharField(max_length=50)
+    road_map = models.CharField(max_length=300)
+    duration = models.IntegerField(default=0)
+    value = models.FloatField(default=0.0)
